eddd48d58a9d9b0331b0ecc57592dedcca392ea4
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-02-24 18:30:04 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 204 ms, Memory: 19396 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -1,23 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        String[] st = br.readLine().split(" ");
+        int N = Integer.parseInt(st[0]);
+        int M = Integer.parseInt(st[1]);
 
         boolean[][] pairs = new boolean[N+1][N+1];
-
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            st = br.readLine().split(" ");
+            int a = Integer.parseInt(st[0]);
+            int b = Integer.parseInt(st[1]);
             pairs[a][b] = true;
             pairs[b][a] = true;
         }
