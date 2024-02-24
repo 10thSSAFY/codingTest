35929055abffaac38be37baf4b4706da12ced91c
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-02-24 18:40:55 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 152 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -1,36 +1,38 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Main {
-
+    private static boolean[][] pair;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] st = br.readLine().split(" ");
-        int N = Integer.parseInt(st[0]);
-        int M = Integer.parseInt(st[1]);
-
-        boolean[][] pairs = new boolean[N+1][N+1];
+        int N = readInt(), M = readInt();
+        pair = new boolean[N][N];
         for (int i = 0; i < M; i++) {
-            st = br.readLine().split(" ");
-            int a = Integer.parseInt(st[0]);
-            int b = Integer.parseInt(st[1]);
-            pairs[a][b] = true;
-            pairs[b][a] = true;
+            int a = readInt(), b = readInt();
+            pair[a - 1][b - 1] = true;
+            pair[b - 1][a - 1] = true;
         }
 
-        int result = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = i + 1; j <= N; j++) {
-                if (pairs[i][j]) continue;
+        int cnt = 0;
+        for (int i = 1; i <= N - 2; i++) {
+            for (int j = i + 1; j <= N - 1; j++) {
+                if (pair[i - 1][j - 1]) continue;
                 for (int k = j + 1; k <= N; k++) {
-                    if (!pairs[j][k] && !pairs[k][i]) {
-                        result++;
-                    }
+                    if (pair[i - 1][k - 1] || pair[j - 1][k - 1]) continue;
+                    cnt++;
                 }
             }
         }
 
-        System.out.println(result);
+        System.out.println(cnt);
+    }
+
+    private static int readInt() throws IOException {
+        int val = 0;
+        int c = System.in.read();
+        while (c <= ' ') {
+            c = System.in.read();
+        } do {
+            val = 10 * val + c - 48;
+        } while ((c = System.in.read()) >= 48 && c <= 57);
+        return val;
     }
 }
