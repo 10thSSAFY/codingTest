dc1836fb0b267c216c50843d7c77a34eaffad1a2
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-02-25 17:20:30 +0900

    [Silver III] Title: 두 수의 합, Time: 252 ms, Memory: 23784 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,22 +1,17 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = readInt();
         int[] A = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            A[i] = readInt();
         }
         Arrays.sort(A);
-        int x = Integer.parseInt(br.readLine());
+        int x = readInt();
 
         int cnt = 0;
         int start = 0;
@@ -33,6 +28,19 @@ public class Main {
             }
         }
         System.out.println(cnt);
-        br.close();
+    }
+
+    private static int readInt() throws IOException {
+        int c;
+        int n = System.in.read() - '0';
+        c = System.in.read();
+        while (c > ' ') {
+            n = 10 * n + c - '0';
+            c = System.in.read();
+        }
+        if (c == '\r') {
+            System.in.read();
+        }
+        return n;
     }
 }
