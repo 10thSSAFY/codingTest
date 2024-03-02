1b7a2c0fdf04a5991e356896e1b779c017799f82
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-03-03 00:32:51 +0900

    Update 3월에 태어난 여성 회원 목록 출력하기.sql

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,6 +1,13 @@
-SELECT member_id, member_name, gender, DATE_FORMAT(date_of_birth, '%Y-%m-%d') AS date_of_birth
-FROM member_profile
-WHERE MONTH(date_of_birth) = 3
-  AND gender = 'W'
-  AND TLNO IS NOT NULL
-ORDER BY member_id ASC;
+SELECT 
+    member_id, 
+    member_name, 
+    gender, 
+    DATE_FORMAT(date_of_birth, '%Y-%m-%d') AS date_of_birth
+FROM 
+    member_profile
+WHERE 
+    MONTH(date_of_birth) = 3
+    AND gender = 'W'
+    AND TLNO IS NOT NULL
+ORDER BY 
+    member_id ASC;
