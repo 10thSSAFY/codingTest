a9a191b6ac064f006b1d2a211eeaaf1b1be9cf08
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-03-05 17:11:51 +0900

    [level 1] Title: 조건에 맞는 도서 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
@@ -0,0 +1,10 @@
+SELECT
+    book_id,
+    DATE_FORMAT(published_date, "%Y-%m-%d") AS published_date
+FROM
+    book
+WHERE
+    YEAR(published_date) = 2021
+    AND category = '인문'
+ORDER  BY
+    published_date;
