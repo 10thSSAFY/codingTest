d4be92b0c42467c02e0c4c20535f04bab833758a
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-03-06 09:50:06 +0900

    [Silver I] Title: 효율적인 해킹, Time: 8892 ms, Memory: 131424 KB -BaekjoonHub

--- Java/백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ Java/백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,61 +1,54 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
-    static int cnt, max = -1;
     static boolean[] visited;
-    static ArrayList<ArrayList<Integer>> list;
+    static ArrayList<Integer>[] list;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
-
-        StringBuilder sb = new StringBuilder();
 
         int N = readInt();
         int M = readInt();
 
-        list = new ArrayList<>();
+        list = new ArrayList[N + 1];
         for (int i = 0; i <= N; i++) {
-            list.add(new ArrayList<>());
+            list[i] = new ArrayList<>();
         }
 
+        arr = new int[N + 1];
         for (int i = 0; i < M; i++) {
             int a = readInt();
             int b = readInt();
-            list.get(b).add(a);
+            list[a].add(b);
         }
 
-        int[] result = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             visited = new boolean[N + 1];
-            cnt = 0;
-            bfs(i);
-            result[i] = cnt;
-            max = Math.max(cnt, max);
+            visited[i] = true;
+            dfs(i);
         }
 
+        int max = 0;
         for (int i = 1; i <= N; i++) {
-            if (result[i] == max) {
+            max = Math.max(arr[i], max);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            if (arr[i] == max) {
                 sb.append(i + " ");
             }
         }
-
         System.out.println(sb);
     }
 
-    private static void bfs(int n) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(n);
-        visited[n] = true;
-        while (!q.isEmpty()) {
-            int nn = q.poll();
-            for (int i : list.get(nn)) {
-                if (!visited[i]) {
-                    q.add(i);
-                    visited[i] = true;
-                    cnt++;
-                }
+    private static void dfs(int n) {
+        for (int lst : list[n]) {
+            if (!visited[lst]) {
+                arr[lst]++;
+                visited[lst] = true;
+                dfs(lst);
             }
         }
     }
