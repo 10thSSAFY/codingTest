05ee77ac691598bc241a177890216c80136e9733
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-03-07 14:57:28 +0900

    [Silver IV] Title: 선택의 기로, Time: 224 ms, Memory: 56260 KB -BaekjoonHub

--- Python/백준/Silver/30970. 선택의 기로/선택의 기로.py
+++ Python/백준/Silver/30970. 선택의 기로/선택의 기로.py
@@ -1,16 +1,21 @@
+import heapq
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-Qlst = []
-Plst = []
+hqQ = []
+hqP = []
 
 for _ in range(N):
     Q, P = map(int, input().split())
-    Qlst.append((Q, 10000 - P))
-    Plst.append((P, 10000 - Q))
+    heapq.heappush(hqQ, (10000 - Q, P))
+    heapq.heappush(hqP, (P, 10000 - Q))
 
-Qlst.sort(reverse=True)
-Plst.sort()
-print(Qlst[0][0], 10000-Qlst[0][1], Qlst[1][0], 10000 - Qlst[1][1])
-print(10000-Plst[0][1], Plst[0][0], 10000-Plst[1][1], Plst[1][0])
+res = heapq.heappop(hqQ)
+print(10000-res[0], res[1], end=' ')
+res = heapq.heappop(hqQ)
+print(10000-res[0], res[1])
+res = heapq.heappop(hqP)
+print(10000-res[1], res[0], end=' ')
+res = heapq.heappop(hqP)
+print(10000-res[1], res[0])
