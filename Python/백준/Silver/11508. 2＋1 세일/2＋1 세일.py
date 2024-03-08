604f832b3389963f03ded4795c3726ba2858a576
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-03-08 09:02:22 +0900

    [Silver IV] Title: 2+1 세일, Time: 96 ms, Memory: 35364 KB -BaekjoonHub

--- Python/백준/Silver/11508. 2＋1 세일/2＋1 세일.py
+++ Python/백준/Silver/11508. 2＋1 세일/2＋1 세일.py
@@ -1,5 +1,5 @@
-import sys
-input = sys.stdin.readline
+from sys import stdin
+input = stdin.readline
 
 N = int(input())
 lst = []
@@ -7,10 +7,6 @@ for _ in range(N):
     lst.append(int(input()))
 
 lst.sort()
-res = 0
-for  _ in range(N//3):
-    res += lst.pop()
-    res += lst.pop()
-    lst.pop()
-
-print(res + sum(lst))
+res = sum(lst)
+free = sum(lst[-3::-3])
+print(res - free)
