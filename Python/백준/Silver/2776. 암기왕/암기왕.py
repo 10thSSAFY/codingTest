9686b4b88b22eebe021446df3bd12952b6190569
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-03-21 21:55:21 +0900

    [Silver IV] Title: 암기왕, Time: 1520 ms, Memory: 185184 KB -BaekjoonHub

--- Python/백준/Silver/2776. 암기왕/암기왕.py
+++ Python/백준/Silver/2776. 암기왕/암기왕.py
@@ -1,22 +1,14 @@
-def main():
-    T = int(input())
-    for _ in range(T):
-        N = int(input())
-        note1 = sorted(list(map(int, input().split())))
-        M = int(input())
-        note2 = list(map(int, input().split()))
-        for num in note2:
-            start, end = 0, N - 1
-            while start <= end:
-                mid = (start + end) // 2
-                if note1[mid] == num:
-                    print(1)
-                    break
-                elif note1[mid] < num:
-                    start = mid + 1
-                else:
-                    end = mid - 1
-            else:
-                print(0)
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    note1 = list(map(int, input().split()))
+    M = int(input())
+    note2 = list(map(int, input().split()))
 
-main()
+    note1 = set(note1)
+
+    for num in note2:
+        if num in note1:
+            print(1)
+        else:
+            print(0)
