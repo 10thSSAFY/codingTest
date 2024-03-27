3ce2d28c04e777b895b04b607bbc752d33cad225
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-03-27 09:53:25 +0900

    [Gold V] Title: 벼락치기, Time: 368 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Gold/14728. 벼락치기/벼락치기.py
+++ Python/백준/Gold/14728. 벼락치기/벼락치기.py
@@ -1,10 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 N, T = map(int, input().split())
 dp = [0] * (T+1)
 
 for _ in range(N):
     time, score = map(int, input().split())
-    for i in range(T, -1, -1):
-        if i >= time:
-            dp[i] = max(dp[i], dp[i-time] + score)
+    for i in range(T, time-1, -1):
+        dp[i] = max(dp[i], dp[i-time] + score)
 
 print(dp[T])
