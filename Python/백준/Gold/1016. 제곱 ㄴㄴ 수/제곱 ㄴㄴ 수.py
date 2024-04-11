88866191a5e4f0d333dff94fce523704b192a993
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-04-12 08:38:04 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 668 ms, Memory: 38932 KB -BaekjoonHub

--- Python/백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ Python/백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -1,12 +1,14 @@
-Min, Max = map(int, input().split())
-Check = [1] * (Max - Min + 1)
+def main():
+    Min, Max = map(int, input().split())
+    Check = [1] * (Max - Min + 1)
+    
+    for i in range(2, int(Max**(0.5) + 1)):
+        pow = i*i
+        start_index = Min // pow
+        if Min % pow != 0:
+            start_index += 1
+        for j in range(start_index, Max // pow + 1):
+            Check[int((j * pow) - Min)] = 0
+    return sum(Check)
 
-for i in range(2, int(Max**(0.5) + 1)):
-    pow = i*i
-    start_index = Min // pow
-    if Min % pow != 0:
-        start_index += 1
-    for j in range(start_index, Max // pow + 1):
-        Check[int((j * pow) - Min)] = 0
-
-print(sum(Check))
+print(main())
