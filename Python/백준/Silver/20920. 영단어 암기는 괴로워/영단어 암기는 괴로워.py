cbf48c51164f3219be37508409b3d165ea928f35
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-04-13 20:33:28 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 296 ms, Memory: 59740 KB -BaekjoonHub

--- Python/백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ Python/백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,19 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-words = dict()
+def main():
+    N, M = map(int, input().split())
+    words = dict()
 
-for _ in range(N):
-    word = input().strip()
-    if len(word) < M:
-        continue
-    if word not in words:
-        words[word] = 1
-    else:
-        words[word] += 1
+    for _ in range(N):
+        word = input().strip()
+        if len(word) < M:
+            continue
+        if word not in words:
+            words[word] = 1
+        else:
+            words[word] += 1
 
-word_list = sorted(words.items(), key=lambda x : (-x[1], -len(x[0]), x[0]))
+    word_list = sorted(words.items(), key=lambda x : (-x[1], -len(x[0]), x[0]))
 
-for w in word_list:
-    print(w[0])
+    for w in word_list:
+        print(w[0])
+
+main()
