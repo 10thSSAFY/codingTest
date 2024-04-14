a8aa739753bcc842bfa7941f37539a9ea285ad0b
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-04-15 00:49:54 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 80 ms, Memory: 33860 KB -BaekjoonHub

--- Python/백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ Python/백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -1,20 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-stack = []
-res = 0
-for _ in range(N):
-    num = int(input())
-    if not stack:
-        stack.append(num)
-    elif stack[-1] > num:
-        res += len(stack)
-        stack.append(num)
-    elif stack[-1] <= num:
-        while stack and stack[-1] <= num:
-            stack.pop()
-        res += len(stack)
-        stack.append(num)
+def main():
+    N = int(input())
+    stack = []
+    res = 0
+    for _ in range(N):
+        num = int(input())
+        if not stack:
+            stack.append(num)
+        elif stack[-1] > num:
+            res += len(stack)
+            stack.append(num)
+        elif stack[-1] <= num:
+            while stack and stack[-1] <= num:
+                stack.pop()
+            res += len(stack)
+            stack.append(num)
 
-print(res)
+    return res
+
+print(main())
