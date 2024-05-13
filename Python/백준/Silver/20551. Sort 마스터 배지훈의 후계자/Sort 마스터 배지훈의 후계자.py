6ae268ba3a8bb16fe453f2d4f44b34faca78fe08
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-05-14 08:52:54 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 884 ms, Memory: 40020 KB -BaekjoonHub

--- Python/백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
+++ Python/백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
@@ -1,25 +1,27 @@
 import sys
 input = sys.stdin.readline
 
+def main():
+    N, M = map(int, input().split())
 
-N, M = map(int, input().split())
+    A_list = []
+    for _ in range(N):
+        A_list.append(int(input()))
+    A_list.sort()
+    A_len = len(A_list)
 
-A_list = []
-for _ in range(N):
-    A_list.append(int(input()))
-A_list.sort()
-A_len = len(A_list)
+    for _ in range(M):
+        num = int(input())
+        s, e, res = 0, A_len - 1, -1
+        while s <= e:
+            m = (s + e) // 2
+            if num == A_list[m]:
+                res = m
+                e = m - 1
+            elif num < A_list[m]:
+                e = m - 1
+            elif A_list[m] < num:
+                s = m + 1
+        print(res)
 
-for _ in range(M):
-    num = int(input())
-    s, e, res = 0, A_len - 1, -1
-    while s <= e:
-        m = (s + e) // 2
-        if num == A_list[m]:
-            res = m
-            e = m - 1
-        elif num < A_list[m]:
-            e = m - 1
-        elif A_list[m] < num:
-            s = m + 1
-    print(res)
+main()
