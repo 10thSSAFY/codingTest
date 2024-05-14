1a5bd98ac172c51c94078b385c171670d8c3c744
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-05-14 09:57:30 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 544 ms, Memory: 60200 KB -BaekjoonHub

--- Python/백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
+++ Python/백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
@@ -1,27 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-def main():
-    N, M = map(int, input().split())
 
-    A_list = []
-    for _ in range(N):
-        A_list.append(int(input()))
-    A_list.sort()
-    A_len = len(A_list)
+N, M = map(int, input().split())
 
-    for _ in range(M):
-        num = int(input())
-        s, e, res = 0, A_len - 1, -1
-        while s <= e:
-            m = (s + e) // 2
-            if num == A_list[m]:
-                res = m
-                e = m - 1
-            elif num < A_list[m]:
-                e = m - 1
-            elif A_list[m] < num:
-                s = m + 1
-        print(res)
+A_list = []
+for _ in range(N):
+    A_list.append(int(input()))
+A_list.sort()
+A_len = len(A_list)
 
-main()
+idx_list = {}
+for (idx, d) in  enumerate(A_list):
+    if d in idx_list:
+        continue
+    idx_list[d] = idx
+
+for _ in range(M):
+    num = int(input())
+    if num in idx_list:
+        print(idx_list[num])
+    else:
+        print(-1)
