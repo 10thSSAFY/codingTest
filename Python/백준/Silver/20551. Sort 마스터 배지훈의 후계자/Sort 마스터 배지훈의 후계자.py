25beb4c3dfdf5ee3edf85f6805f327d3c327eed6
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-05-14 09:58:11 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 440 ms, Memory: 60200 KB -BaekjoonHub

--- Python/백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
+++ Python/백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
@@ -2,23 +2,26 @@ import sys
 input = sys.stdin.readline
 
 
-N, M = map(int, input().split())
+def main():
+    N, M = map(int, input().split())
 
-A_list = []
-for _ in range(N):
-    A_list.append(int(input()))
-A_list.sort()
-A_len = len(A_list)
+    A_list = []
+    for _ in range(N):
+        A_list.append(int(input()))
+    A_list.sort()
+    A_len = len(A_list)
 
-idx_list = {}
-for (idx, d) in  enumerate(A_list):
-    if d in idx_list:
-        continue
-    idx_list[d] = idx
+    idx_list = {}
+    for (idx, d) in  enumerate(A_list):
+        if d in idx_list:
+            continue
+        idx_list[d] = idx
 
-for _ in range(M):
-    num = int(input())
-    if num in idx_list:
-        print(idx_list[num])
-    else:
-        print(-1)
+    for _ in range(M):
+        num = int(input())
+        if num in idx_list:
+            print(idx_list[num])
+        else:
+            print(-1)
+
+main()
