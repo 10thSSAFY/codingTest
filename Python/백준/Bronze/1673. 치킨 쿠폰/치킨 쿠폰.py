2e4482848aa23505b120e43c2259aa6cd351bcda
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-06-08 19:26:07 +0900

    [Bronze II] Title: 치킨 쿠폰, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1673. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        n, k = map(int, input().split())
+        res = 0 
+        res += n
+        while n // k:
+            res += n // k
+            n = n // k + n % k
+        print(res)
+    except:
+        break
