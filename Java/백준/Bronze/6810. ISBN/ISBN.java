9b03ddffb9df36d63876df301f5261a4639cd0ee
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-07-15 19:40:58 +0900

    [Bronze IV] Title: ISBN, Time: 120 ms, Memory: 15828 KB -BaekjoonHub

---
+++ Java/백준/Bronze/6810. ISBN/ISBN.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        System.out.println("The 1-3-sum is " + (91 + a + b * 3 + c));
+    }
+}
