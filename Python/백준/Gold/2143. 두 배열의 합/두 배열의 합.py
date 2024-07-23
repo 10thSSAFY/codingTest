38b3f3a7fb2a1ad02f142638e8e822715f37d79d
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-07-23 17:08:41 +0900

    [Gold III] Title: 두 배열의 합, Time: 360 ms, Memory: 73108 KB -BaekjoonHub

--- Python/백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ Python/백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -1,38 +1,24 @@
 T = int(input())
-An = int(input())
-A = list(map(int, input().split()))
-Bn = int(input())
-B = list(map(int, input().split()))
+n = int(input()); A = list(map(int, input().split()))
+m = int(input()); B = list(map(int, input().split()))
 
-sumA = [0] * (An + 1)
-for i in range(An):
-    sumA[i+1] = sumA[i] + A[i]
+prefix = {}
 
-sumB = [0] * (Bn + 1)
-for i in range(Bn):
-    sumB[i+1] = sumB[i] + B[i]
-
-dictA = {}
-for e in range(1, An + 1):
-    for s in range(e):
-        prefix = sumA[e] - sumA[s]
-        if prefix in dictA:
-            dictA[prefix] += 1
+for s in range(n):
+    sumA = 0
+    for e in range(s, n):
+        sumA += A[e]
+        if sumA in prefix:
+            prefix[sumA] += 1
         else:
-            dictA[prefix] = 1
-
-dictB = {}
-for e in range(1, Bn + 1):
-    for s in range(e):
-        prefix = sumB[e] - sumB[s]
-        if prefix in dictB:
-            dictB[prefix] += 1
-        else:
-            dictB[prefix] = 1
+            prefix[sumA] = 1
 
 result = 0
-for preNum, cnt in dictA.items():
-    if T - preNum in dictB:
-        result += dictA[preNum] * dictB[T - preNum]
+for s in range(m):
+    sumB = 0
+    for e in range(s, m):
+        sumB += B[e]
+        if T - sumB in prefix:
+            result += prefix[T - sumB]
 
 print(result)
