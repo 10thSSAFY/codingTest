142801420833c39d6b29c38e0d16911d7553c1c4
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-08-18 20:55:17 +0900

    [Gold I] Title: 외판원 순회, Time: 996 ms, Memory: 42364 KB -BaekjoonHub

---
+++ Python/백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -0,0 +1,26 @@
+def solution(node, visited):
+    if dp[node][visited] != 0:
+        return dp[node][visited]
+
+    if visited == (1 << (N - 1)) - 1:
+        if not W[node][0]:
+            return float('inf')
+        else:
+            return W[node][0]
+
+    res = float('inf')
+    for i in range(1, N):
+        if not W[node][i] or visited & (1 << i - 1):
+            continue
+        cost = W[node][i] + solution(i, visited | (1 << (i - 1)))
+        res = min(res, cost)
+    dp[node][visited] = res
+    return res
+
+
+N = int(input())
+W = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0 for _ in range(1 << N - 1)] for _ in range(N)]
+result = solution(0, 0)
+print(result)
