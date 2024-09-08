686694c4460bd5aa9bf548fef1a6f80b1ce9d4cb
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-09-08 18:04:00 +0900

    [Silver I] Title: 오목, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/2615. 오목/오목.py
@@ -0,0 +1,48 @@
+def check(R, C):
+    global win, flag, ans
+    color = arr[R][C]
+
+    for d in D:
+        cnt = 1
+        for i in range(5):
+            dr, dc = d[i][0], d[i][1]
+            nr, nc = R + dr, C + dc
+            if not (0 <= nr < 19 and 0 <= nc < 19):
+                break
+            if arr[nr][nc] != color:
+                break
+            cnt += 1
+
+        if cnt == 5 and color != arr[R + d[5][0]][C + d[5][1]]:
+            flag = True
+            win = color
+            ans = (R, C)
+            return
+
+
+D = [((-1, 1), (-2, 2), (-3, 3), (-4, 4), (-5, 5), (1,  -1)),
+     ((1,  0), (2,  0), (3,  0), (4,  0), (5,  0), (-1,  0)),
+     ((1,  1), (2,  2), (3,  3), (4,  4), (5,  5), (-1, -1)),
+     ((0,  1), (0,  2), (0,  3), (0,  4), (0,  5), ( 0, -1))]
+
+
+arr = [list(map(int, input().split())) for _ in range(19)]
+
+win = False
+ans = False
+flag = False
+
+for r in range(19):
+    for c in range(19):
+        if arr[r][c] != 0:
+            check(r, c)
+        if flag:
+            break
+    if flag:
+        break
+
+if win:
+    print(win)
+    print(ans[0] + 1, ans[1] + 1)
+else:
+    print(0)
