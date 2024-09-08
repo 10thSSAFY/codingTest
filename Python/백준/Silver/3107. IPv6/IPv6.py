b28437cdfcf9343148d3b3177e255f51a18102d7
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-09-08 17:16:18 +0900

    [Silver I] Title: IPv6, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/3107. IPv6/IPv6.py
+++ Python/백준/Silver/3107. IPv6/IPv6.py
@@ -1,47 +1,22 @@
-IPv6 = input()
-N = len(IPv6)
+lst = list(input().split(":"))
+tmp = 8
+ans = ""
 
-result = [[] for _ in range(8)]
-idx = 0
-hexa = ''
-for i in range(N):
-    if IPv6[i : i+2] == '::':
-        break
+for i in lst:
+    if i != "":
+        tmp -= 1
 
-    if IPv6[i] == ':':
-        result[idx].append(hexa)
-        hexa = ''
-        idx += 1
+flag = False
+for i in lst:
+    if i == "":
+        if not flag:
+            flag = True
+            for j in range(tmp):
+                ans += "0000:"
         continue
 
-    hexa = hexa + IPv6[i]
+    for j in range(4-len(i)):
+        ans += "0"
+    ans += i + ":"
 
-if hexa:
-    result[idx].append(hexa)
-
-if idx != 7:
-    idx = 7
-    hexa = ''
-    for i in range(N, -1, -1):
-        if IPv6[i-2 : i] == '::':
-            break
-
-        if IPv6[i-1] == ':':
-            result[idx].append(hexa)
-            hexa = ''
-            idx -= 1
-            continue
-
-        hexa = IPv6[i-1] + hexa
-
-if hexa:
-    result[idx].append(hexa)
-
-for i in range(8):
-    if not result[i]:
-        print('0000', end='')
-    else:
-        hexa = result[i][0]
-        print(f'{hexa:0>4}', end='')
-    if i != 7:
-        print(':', end='')
+print(ans[:-1])
