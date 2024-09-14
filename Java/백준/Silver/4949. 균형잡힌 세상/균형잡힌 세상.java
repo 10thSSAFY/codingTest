5af1816fa316b41e2dde9a71e50d416af3da4ebc
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-09-14 20:38:43 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 172 ms, Memory: 18688 KB -BaekjoonHub

---
+++ Java/백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s;
+
+        while (true) {
+            s = br.readLine();
+            if (s.equals(".")) {
+                break;
+            }
+            sb.append(solution(s)).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    public static String solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }
+}
