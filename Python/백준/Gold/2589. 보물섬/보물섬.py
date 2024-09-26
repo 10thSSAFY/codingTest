7a4ac05811e6b18b2b876ee0ed76a3d9bc0ddda0
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-09-27 00:04:55 +0900

    [Gold V] Title: 보물섬, Time: 400 ms, Memory: 34104 KB -BaekjoonHub

--- Python/백준/Gold/2589. 보물섬/보물섬.py
+++ Python/백준/Gold/2589. 보물섬/보물섬.py
@@ -1,31 +1,36 @@
+from sys import stdin
 from collections import deque
 
-def bfs(r, c):
-    q = deque([(r, c)])
-    visited = [[False] * M for _ in range(N)]
-    visited[r][c] = 1
-    cnt = 0
-    while q:
-        (r, c) = q.popleft()
-        for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-            nr, nc = r + dr, c + dc
-            if not (0 <= nr < N and 0 <= nc < M):
-                continue
-            if arr[nr][nc] == 'L' and not visited[nr][nc]:
-                visited[nr][nc] = visited[r][c] + 1
-                cnt = max(cnt, visited[nr][nc])
-                q.append((nr, nc))
-    return cnt - 1
-
-
-
-N, M = map(int, input().split())
-arr = [list(input()) for _ in range(N)]
+N, M = map(int, stdin.readline().split())
+arr = [input() for _ in range(N)]
+starts_1, starts_2 = [], []
+for r in range(N):
+    for c in range(M):
+        if arr[r][c]=="L":
+            cnt = 0
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == "L":
+                    cnt += 1
+            if cnt == 1:
+                starts_1.append((r, c))
+            elif cnt == 2:
+                starts_2.append((r, c))
 
 result = 0
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 'L':
-            result = max(result, bfs(i, j))
+starts = starts_1 if starts_1 else starts_2
+while starts:
+    sr, sc = starts.pop()
+    q = deque([(0, sr, sc)])
+    visited = [[0] * M for _ in range(N)]
+    visited[sr][sc] = 1
+    while q:
+        cnt, row, col = q.popleft()
+        result = max(result, cnt)
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nr, nc = row + dr, col + dc
+            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == "L" and not visited[nr][nc]:
+                visited[nr][nc] = 1
+                q.append((cnt + 1, nr, nc))
 
 print(result)
