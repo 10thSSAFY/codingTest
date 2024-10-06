d67d5136131ea5b42685702719186d0487e4c8b2
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-10-06 19:04:53 +0900

    [Silver IV] Title: 30, Time: 4792 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/10610. 30/30.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+nums = [0] * 10
+curr = 0
+num = N
+while num > 9:
+    curr += num % 10
+    nums[num % 10] += 1
+    num //= 10
+
+curr += num
+nums[num] += 1
+
+result = ''
+if curr % 3 or not nums[0]:
+    print(-1)
+else:
+    for i in range(9, -1, -1):
+        result += str(i) * nums[i]
+
+print(result)
