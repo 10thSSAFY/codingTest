c56f6cd57fba7785613226c72239474a4f77c901
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-10-11 21:54:52 +0900

    [Bronze II] Title: 손익분기점, Time: 96 ms, Memory: 14148 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if (C <= B) {
+            System.out.println("-1");
+        } else {
+            System.out.println(A / (C - B) + 1);
+        }
+    }
+}
