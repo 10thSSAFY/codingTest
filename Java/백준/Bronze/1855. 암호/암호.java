067a62b6fc94a0c69ee258ae454b65179dea73f2
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-10-20 23:56:40 +0900

    [Bronze I] Title: 암호, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1855. 암호/암호.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int k,sz,i,j;
+        k = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        sz = str.length();
+        for (i = 0; i < k; i++)
+            for (j = 0; j < sz / k; j++) sb.append(str.charAt(j % 2 == 0 ? j * k + i : j * k + k - 1 - i));
+        
+        System.out.println(sb);
+    }
+}
