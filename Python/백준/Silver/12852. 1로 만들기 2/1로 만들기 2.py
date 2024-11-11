91040b72145bc7f3e78e50ab91a1558d42501647
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-11-11 23:43:08 +0900

    [Silver I] Title: 1로 만들기 2, Time: 2080 ms, Memory: 379336 KB -BaekjoonHub

---
+++ Python/백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [[0, []] for _ in range(N + 1)]
+dp[1][0] = 0
+dp[1][1] = [1]
+
+for i in range(2, N + 1):
+    dp[i][0] = dp[i - 1][0] + 1
+    dp[i][1] = dp[i - 1][1] + [i]
+    if i % 3 == 0 and dp[i // 3][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i // 3][0] + 1
+        dp[i][1] = dp[i // 3][1] + [i]
+    if i % 2 == 0 and dp[i // 2][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i // 2][0] + 1
+        dp[i][1] = dp[i // 2][1] + [i]
+
+print(dp[N][0])
+print(*reversed(dp[N][1]))
