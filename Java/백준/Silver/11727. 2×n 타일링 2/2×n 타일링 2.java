0db97c4a1b70d5316805275d12b01f83e7cff8c4
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-11-15 22:43:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ Java/백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+        }
+        
+        System.out.println(dp[N]);
+    }
+}
