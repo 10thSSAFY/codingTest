8820a593a0dbe5dde5301f40944501dab4f54bea
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-11-30 17:48:11 +0900

    [Gold III] Title: 나무 재테크, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ Python/백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,54 +1,54 @@
 import sys
 input = sys.stdin.readline
 
-dr = (-1, -1, -1, 0, 0, 1, 1, 1)
-dc = (-1, 0, 1, -1, 1, -1, 0, 1)
+def solution():
+    for r in range(N):
+        for c in range(N):
+            if not trees[r][c]:
+                continue
+            dead = 0
+            grow = {}
+            for age, cnt in sorted(trees[r][c].items()):
+                alive = min(soil[r][c] // age, cnt)
+                dead += age // 2 * (cnt - alive)
+                if alive:
+                    soil[r][c] -= alive * age
+                    grow[age + 1] = alive
+            soil[r][c] += dead
+            trees[r][c] = grow
+
+    for r in range(N):
+        for c in range(N):
+            tmp = 0
+            for age in trees[r][c]:
+                if age % 5 != 0:
+                    continue
+                tmp += trees[r][c][age]
+            if tmp:
+                for dr, dc in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                    nr, nc = r + dr, c + dc
+                    if not (0 <= nr < N and 0 <= nc < N):
+                        continue
+                    trees[nr][nc][1] = trees[nr][nc].get(1, 0) + tmp
+            soil[r][c] += A[r][c]
+
 
 N, M, K = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
-seeds = [list(map(int, input().split())) for _ in range(M)]
 
 soil = [[5] * N for _ in range(N)]
 trees = [[dict() for _ in range(N)] for _ in range(N)]
-for x, y, z in seeds:
-    trees[x-1][y-1][z] = 1
+
+for _ in range(M):
+    x, y, z = map(int, input().split())
+    trees[x - 1][y - 1][z] = 1
 
 for _ in range(K):
-    for i in range(N):
-        for j in range(N):
-            if not trees[i][j]:
-                continue
-            soil_from_dead = 0
-            grow_trees = {}
-            for age, cnt in sorted(trees[i][j].items()):
-                alive = min(soil[i][j] // age, cnt)
-                soil_from_dead += age // 2 * (cnt - alive)
-                if alive:
-                    soil[i][j] -= alive * age
-                    grow_trees[age+1] = alive
-            soil[i][j] += soil_from_dead
-            trees[i][j] = grow_trees
+    solution()
 
-    for i in range(N):
-        for j in range(N):
-            tmp = 0
-            for age in trees[i][j]:
-                if age % 5 != 0:
-                    continue
-                tmp += trees[i][j][age]
-            if tmp:
-                for d in range(8):
-                    nr = i + dr[d]
-                    nc = j + dc[d]
-                    if nr < 0 or nr >= N or nc < 0 or nc >= N:
-                        continue
-                    trees[nr][nc][1] = trees[nr][nc].get(1, 0) + tmp
+result = 0
+for r in range(N):
+    for c in range(N):
+        result += sum(trees[r][c].values())
 
-            soil[i][j] += A[i][j]
-
-ans = 0
-for i in range(N):
-    for j in range(N):
-        ans += sum(trees[i][j].values())
-
-print(ans)
+print(result)
