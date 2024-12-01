a16f8cd59d1984ccc7796869cf92240a5a4d25b0
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-12-01 17:06:53 +0900

    [Silver I] Title: IOIOI, Time: 316 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Silver/5525. IOIOI/IOIOI.py
+++ Python/백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,14 +1,26 @@
-N = int(input())
-M = int(input())
-S = input()
+import sys
+input = sys.stdin.readline
 
-P = 'I'
-for _ in range(N):
-    P += 'OI'
 
-result = 0
-for i in range(M - N * 2):
-    if S[i:i+(N * 2 + 1)] == P:
-        result += 1
+N = int(input().strip())
+M = int(input().strip())
+S = input().strip()
 
-print(result)
+P = 'IOI'
+
+cnt = 0
+i = 0
+answer = 0
+
+while i < (M - 1):
+    if S[i : i+3] == P:
+        i += 2
+        cnt += 1
+        if cnt == N:
+            answer += 1
+            cnt -= 1
+    else:
+        i += 1
+        cnt = 0
+
+print(answer)
