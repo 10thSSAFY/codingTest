a5fc4b1c07b8b12afd5b27b265443502b460483d
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2024-12-20 19:30:27 +0900

    [Gold IV] Title: 괄호, Time: 808 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Gold/10422. 괄호/괄호.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+dp = [0 for _ in range(5001)]
+dp[0] = 1
+
+for n in range(2, 5001, 2):
+    for i in range(2, n + 1, 2):
+        dp[n] += dp[i - 2] * dp[n - i]
+    dp[n] %= 1000000007
+
+for _ in range(T):
+    L = int(input())
+    print(dp[L])
