8fc2dd15a236ea3ece12ac75d80bb0da86474645
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-01-09 06:51:08 +0900

    [Gold V] Title: 수 고르기, Time: 152 ms, Memory: 36264 KB -BaekjoonHub

--- Python/백준/Gold/2230. 수 고르기/수 고르기.py
+++ Python/백준/Gold/2230. 수 고르기/수 고르기.py
@@ -9,15 +9,15 @@ for _ in range(N):
     A.append(int(input()))
 A.sort()
 
-s, e = 0, 1
+s, e = 0, 0
 result = float('inf')
 while e < N:
     num = A[e] - A[s]
     if num >= M:
         result = min(result, num)
         s += 1
-        if s == e:
-            e += 1
+        if s > e:
+            e = s
     else:
         e += 1
 
