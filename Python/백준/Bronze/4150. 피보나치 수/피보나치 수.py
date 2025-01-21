bda4c7dd6f2d928d94f5dc134b6cf5d0155724fe
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-01-21 22:41:10 +0900

    [Bronze I] Title: 피보나치 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/4150. 피보나치 수/피보나치 수.py
+++ Python/백준/Bronze/4150. 피보나치 수/피보나치 수.py
@@ -1,9 +1,4 @@
-N = int(input())
-
-if N == 1: print(1)
-elif N == 2: print(1)
-else:
-    a, b, r = 1, 1, 2
-    for _ in range(N - 3):
-        a, b, r = b, r, b + r
-    print(r)
+N=int(input())
+a,b,r=1,1,1
+for _ in range(N-2):a,b,r=b,r,b+r
+print(r)
