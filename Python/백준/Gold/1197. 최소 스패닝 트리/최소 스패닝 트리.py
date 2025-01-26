86a674bec6b999786a777a09e8333ba9fb4f0067
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-01-26 20:20:42 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 304 ms, Memory: 50664 KB -BaekjoonHub

--- Python/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ Python/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,34 +1,37 @@
 import sys
+sys.setrecursionlimit(10 ** 8)
 input = sys.stdin.readline
 
 
-def union(a, b):
-    a, b = parent[a], parent[b]
-    if a > b:
-        parent[a] = b
+def union(A, B):
+    A = find(A)
+    B = find(B)
+    if A < B:
+        parent[B] = A
     else:
-        parent[b] = a
+        parent[A] = B
+
 
 def find(x):
-    if parent[x] == x:
-        return x
-    parent[x] = find(parent[x])
+    if parent[x] != x:
+        parent[x] = find(parent[x])
     return parent[x]
 
 
 V, E = map(int, input().split())
-parent = [i for i in range(V+1)]
-lst = []
-for _ in range(E):
-    A, B, C = map(int, input().split())
-    lst.append((C, A, B))
-
-lst.sort()
-
+parent = [i for i in range(V + 1)]
+tree = []
 result = 0
-for (cost, nodeA, nodeB) in lst:
-    if find(nodeA) != find(nodeB):
-        union(nodeA, nodeB)
-        result += cost
+
+for _ in range(E):
+    A, B, W = map(int, input().split())
+    tree.append((W, A, B))
+tree.sort()
+
+for edge in tree:
+    (W, A, B) = edge
+    if find(A) != find(B):
+        union(A, B)
+        result += W
 
 print(result)
