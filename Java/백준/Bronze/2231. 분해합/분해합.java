f90976e6d7312e3cdcd749a768b3d825e8337cb5
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-02-25 15:31:54 +0900

    [Bronze II] Title: 분해합, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- Java/백준/Bronze/2231. 분해합/분해합.java
+++ Java/백준/Bronze/2231. 분해합/분해합.java
@@ -1,29 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    
+
+    // 자릿수의 합을 계산하는 함수
+    private static int digitSum(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) throws Exception {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int M = 0;
 
-        for(int i = 1; i <= N; i++) {
-            int number = i;
-            int res = 0;
+        int result = 0;
 
-            while(number > 0){
-                res += number % 10;
-                number /= 10;
-            }
-
-            if(res + i == N){
-                M = i;
+        for (int i = Math.max(1, N - String.valueOf(N).length() * 9); i < N; i++) {
+            int decompositionSum = i + digitSum(i);  // 분해합 계산
+            if (decompositionSum == N) {
+                result = i;
                 break;
             }
         }
-        System.out.println(M);
+
+        System.out.println(result);
     }
 }
