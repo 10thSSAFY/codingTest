b5aeb9c4fd996523faf29d96d8c125c2fc86817d
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-02-27 16:16:25 +0900

    [Bronze II] Title: 펫, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1362. 펫/펫.py
@@ -0,0 +1,29 @@
+cnt = 0
+while 1:
+    death = 0
+    cnt += 1
+
+    o, w = map(int,input().split())
+    if o == w == 0:
+        break
+
+    while 1:
+        a,b = input().split()
+        if a == '#' and b == '0':
+            break
+        b = int(b)
+        if a == 'F':
+            w = w + b
+        elif a == 'E':
+            w = w - b
+        if w <= 0 :
+            death = 1
+    print(cnt,end=' ')
+
+    if death == 1:
+        print("RIP")
+    else:
+        if o/2 < w < o*2:
+            print(":-)")
+        else:
+            print(":-(")
