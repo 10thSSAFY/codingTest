539045c58f46aaafe94f23ddd91579306fd37db0
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-03-04 21:18:01 +0900

    [Silver III] Title: 선물, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1166. 선물/선물.py
@@ -0,0 +1,12 @@
+N, L, W, H = map(int, input().split())
+S, E = 0, max(L, W, H)
+
+for _ in range(10000):
+    M = (S + E) / 2
+    count = (L // M) * (W // M) * (H // M)
+    if count >= N:
+        S = M
+    else:
+        E = M
+
+print("%.10f" %(E))
