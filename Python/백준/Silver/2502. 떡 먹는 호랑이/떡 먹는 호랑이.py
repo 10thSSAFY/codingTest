b05d93d13d5c76a2d9ac888c016a5e581a1f5cb6
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-03-09 20:43:56 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
@@ -0,0 +1,23 @@
+D, K = map(int, input().split())
+
+d = [(-1, -1)] * 31
+
+d[1] = (1, 0)
+d[2] = (0, 1)
+for i in range(3, D+1):
+    d[i] = (d[i - 1][0] + d[i - 2][0], d[i - 1][1] + d[i - 2][1])
+
+a = d[D][0]
+b = d[D][1]
+
+n, m = 1, 1
+while True:
+    if a * n + b * m == K:
+        print(n)
+        print(m)
+        break
+    elif a * n + b * m < K:
+        m += 1
+    else:
+        n += 1
+        m = 1
