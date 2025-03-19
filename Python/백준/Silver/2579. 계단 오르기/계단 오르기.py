7275fabe86d940ee30997a5a461b63ec1d7b529e
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-03-19 21:33:26 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ Python/백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,14 +1,32 @@
-N = int(input())
-lst = [0] * 301
-for i in range(N):
-    lst[i] = int(input())
+input = __import__('sys').stdin.readline
 
-dp = [0] * 301
-dp[0] = lst[0]
-dp[1] = lst[0] + lst[1]
-dp[2] = max(lst[0] + lst[2], lst[1] + lst[2])
 
-for i in range(3, N):
-    dp[i] = max(lst[i] + dp[i-2], lst[i] + lst[i-1] + dp[i-3])
+def max_score_of_stairs(stairs):
+    n = len(stairs)
 
-print(dp[N-1])
+    if n == 0:
+        return 0
+    elif n == 1:
+        return stairs[0]
+    elif n == 2:
+        return stairs[0] + stairs[1]
+
+    # 각 계단을 오르는 최대값을 저장할 변수
+    dp = [0] * n
+    # 첫번째 계단까지의 최대값
+    dp[0] = stairs[0]
+    # 두번째 계단까지의 최대값
+    dp[1] = stairs[0] + stairs[1]
+    # 세번째 계단까지의 최대값
+    dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
+
+    # 네번째부터 마지막까지 반복하며, 각 계단의 최대값 구하기
+    for i in range(3, n):
+        dp[i] = max(dp[i - 2], dp[i - 3] + stairs[i - 1]) + stairs[i]
+
+    return dp[-1]
+
+
+# 계단 점수를 입력받습니다.
+stairs = [int(input()) for _ in range(int(input()))]
+print(max_score_of_stairs(stairs))
