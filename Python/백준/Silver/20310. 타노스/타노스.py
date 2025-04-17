239fafdb837b6f32a9e8be231f4b60b2951c62d4
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-04-17 20:13:35 +0900

    [Silver III] Title: 타노스, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Silver/20310. 타노스/타노스.py
+++ Python/백준/Silver/20310. 타노스/타노스.py
@@ -1,5 +1,23 @@
 S = list(input().rstrip())
-n = len(S) // 2
-m = S.count('1') // 2
-result = '0' * (n - m) + '1' * m
-print(result)
+n = S.count('0')
+m = S.count('1')
+
+check = 0
+for num in S:
+    if check == m // 2:
+        break
+    if num == '1':
+        S.remove(num)
+        check += 1
+
+check = 0
+S = S[::-1]
+for num in S:
+    if check == n // 2:
+        break
+    if num == '0':
+        S.remove(num)
+        check += 1
+
+for num in S[::-1]:
+    print(num, end='')
