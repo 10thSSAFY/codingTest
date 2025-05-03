cb194191cb715beaa8052b45acabf0f94342e9d1
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2025-05-03 22:38:41 +0900

    [Gold V] Title: 배, Time: 296 ms, Memory: 16480 KB -BaekjoonHub

---
+++ Java/백준/Gold/1092. 배/배.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    
+    public static void main(String []args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        N = Integer.parseInt(br.readLine());
+        List<Integer> crane = new ArrayList<>();
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            crane.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        M = Integer.parseInt(br.readLine());
+        List<Integer> box = new ArrayList<>();
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            box.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        crane.sort(Collections.reverseOrder());
+        box.sort(Collections.reverseOrder());
+        
+        if (crane.get(0) < box.get(0)) {
+            System.out.println(-1);
+            return;
+        }
+        
+        int result = 0;
+        while (!box.isEmpty()) {
+            int boxIdx = 0, craneIdx = 0;
+            
+            while (craneIdx < N) {
+                if (boxIdx == box.size())
+                    break;
+                else if(crane.get(craneIdx) >= box.get(boxIdx)) {
+                    box.remove(boxIdx);
+                    craneIdx++;
+                } else
+                    boxIdx++;
+            }
+            result++;
+        }
+        
+        System.out.println(result);
+    }
+}
